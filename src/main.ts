179fc8b415f7b66502cb32bf5039fb46710d24a9
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2023-03-02 22:42:57 -0600

    Add stackblitz polyfills

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import './polyfills';
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
